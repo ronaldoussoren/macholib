270a88075ca3c230a13e5f096377236d54e29319
Ronald Oussoren <ronaldoussoren@mac.com>
2010-07-27 19:00:03 +0000

    Move news to setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,30 @@ references in Mach-O headers to be @executable_path relative.
 
 Though this tool targets a platform specific file format, it is pure python
 code that is platform and endian independent.
+
+NEWS
+====
+
+macholib 1.3
+------------
+
+macholib 1.3 is a feature release.
+
+Features:
+
+- Experimental Python 3.x support
+
+  This version contains lightly tested support for Python 3.
+
+macholib 1.2.2
+--------------
+
+macholib 1.2.2 is a bugfix release.
+
+Bug fixes:
+
+- Macholib should work better with 64-bit code
+  (patch by Marc-Antoine Parent)
 """
 
 CLASSIFIERS = filter(None, map(str.strip,
@@ -27,6 +51,7 @@ CLASSIFIERS = filter(None, map(str.strip,
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Programming Language :: Python
+Programming Language :: Python :: 2
 Programming Language :: Python :: 3
 Operating System :: MacOS :: MacOS X
 Topic :: Software Development :: Libraries :: Python Modules
