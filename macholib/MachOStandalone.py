2ca775ae4a67a0a8feb6e46ef5bba1e5765d4813
Ronald Oussoren <ronaldoussoren@mac.com>
2012-10-17 14:31:13 +0200

    Avoid crash when a library is linked two under two names (real name and a symlink)
    See <https://bitbucket.org/ronaldoussoren/py2app/issue/65/wx-crashes-pointer-being-freed-was-not>
    for more information.

--- macholib/MachOStandalone.py
+++ macholib/MachOStandalone.py
@@ -67,7 +67,14 @@ class MachOStandalone(object):
             return res
 
     def copy_dylib(self, filename):
-        dest = os.path.join(self.dest, os.path.basename(filename))
+        # When the filename is a symlink use the basename of the target of the link
+        # as the name in standalone bundle. This avoids problems when two libraries
+        # link to the same dylib but using different symlinks.
+        if os.path.islink(filename):
+            dest = os.path.join(self.dest, os.path.basename(os.path.realpath(filename)))
+        else:
+            dest = os.path.join(self.dest, os.path.basename(filename))
+
         if not os.path.exists(dest):
             self.mergecopy(filename, dest)
         return dest
