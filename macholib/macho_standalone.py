387aef96178f6d80c55451fc42dfa14bf9ac8e77
Ronald Oussoren <ronaldoussoren@mac.com>
2012-10-17 10:26:18 +0200

    Fix check in macho_standalone

--- macholib/macho_standalone.py
+++ macholib/macho_standalone.py
@@ -7,10 +7,11 @@ from macholib.MachOStandalone import MachOStandalone
 from macholib.util import strip_files
 
 def standaloneApp(path):
-    if not os.path.isdir(path) and os.path.exists(
-            os.path.join(path, 'Contents')):
-        raise SystemExit('%s: %s does not look like an app bundle'
+    if not (os.path.isdir(path) and os.path.exists(
+            os.path.join(path, 'Contents'))):
+        print('%s: %s does not look like an app bundle'
             % (sys.argv[0], path))
+        sys.exit(1)
     files = MachOStandalone(path).run()
     strip_files(files)
 
