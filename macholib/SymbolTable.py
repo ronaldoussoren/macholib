9959713b3ad9e81375f429ed8d37a16145d4148b
Christian Klein <chris@5711.org>
2014-10-24 16:52:22 +0200

    Fix for SymbolTable
    SymbolTable can now load the symbol table of 64 bit and fat binaries.
    The signature of __init__ changed to be able to handle a certain mach-o header.

--- macholib/SymbolTable.py
+++ macholib/SymbolTable.py
@@ -1,61 +1,75 @@
 """
 Class to read the symbol table from a Mach-O header
 """
+from __future__ import with_statement
 
 from macholib.mach_o import *
 
 __all__ = ['SymbolTable']
 
-# XXX: Does not support 64-bit, probably broken anyway
 
 class SymbolTable(object):
-    def __init__(self, macho, openfile=None):
+    # TODO: HEADER, nicht File! - oder eine pro header.
+    def __init__(self, macho, header, openfile=None):
         if openfile is None:
             openfile = open
-        self.macho = macho.headers[0]
-        self.symtab = macho.getSymbolTableCommand()
-        self.dysymtab = macho.getDynamicSymbolTableCommand()
-        fh = openfile(self.macho.filename, 'rb')
-        try:
-            if self.symtab is not None:
-                self.readSymbolTable(fh)
-            if self.dysymtab is not None:
-                self.readDynamicSymbolTable(fh)
-        finally:
-            fh.close()
+        self.macho_header = header
+        with openfile(macho.filename, 'rb') as fh:
+            self.symtab = header.getSymbolTableCommand()
+            self.dysymtab = header.getDynamicSymbolTableCommand()
+            try:
+                if self.symtab is not None:
+                    self.nlists = self.readSymbolTable(fh)
+                    print self.nlists
+
+                if self.dysymtab is not None:
+                    self.readDynamicSymbolTable(fh)
+            except:
+                raise
 
     def readSymbolTable(self, fh):
         cmd = self.symtab
-        fh.seek(cmd.stroff)
+        fh.seek(self.macho_header.offset + cmd.stroff)
         strtab = fh.read(cmd.strsize)
-        fh.seek(cmd.symoff)
+        fh.seek(self.macho_header.offset + cmd.symoff)
         nlists = []
+
+        if self.macho_header.MH_MAGIC in [MH_MAGIC_64, MH_CIGAM_64]:
+            cls = nlist_64
+            print 64
+        else:
+            cls = nlist
+            print 32
+
         for i in xrange(cmd.nsyms):
-            cmd = nlist.from_fileobj(fh)
+            cmd = cls.from_fileobj(fh, _endian_=self.macho_header.endian)
             if cmd.n_un == 0:
                 nlists.append((cmd, ''))
             else:
                 nlists.append((cmd, strtab[cmd.n_un:strtab.find(b'\x00', cmd.n_un)]))
-        self.nlists = nlists
+        return nlists
 
     def readDynamicSymbolTable(self, fh):
         cmd = self.dysymtab
         nlists = self.nlists
+
         self.localsyms = nlists[cmd.ilocalsym:cmd.ilocalsym+cmd.nlocalsym]
         self.extdefsyms = nlists[cmd.iextdefsym:cmd.iextdefsym+cmd.nextdefsym]
         self.undefsyms = nlists[cmd.iundefsym:cmd.iundefsym+cmd.nundefsym]
-        #if cmd.tocoff == 0:
-        #    self.toc = None
-        #else:
-        #    self.toc = self.readtoc(fh, cmd.tocoff, cmd.ntoc)
-        #if cmd.modtaboff == 0:
-        #    self.modtab = None
-        #else:
-        #    self.modtab = self.readmodtab(fh, cmd.modtaboff, cmd.nmodtab)
-        if cmd.extrefsymoff == 0:
-            self.extrefsym = None
+        if cmd.tocoff == 0:
+           self.toc = None
         else:
-            self.extrefsym = self.readsym(fh, cmd.extrefsymoff, cmd.nextrefsyms)
+           self.toc = self.readtoc(fh, cmd.tocoff, cmd.ntoc)
+
+        # if cmd.modtaboff == 0:
+        #    self.modtab = None
+        # else:
+        #    self.modtab = self.readmodtab(fh, cmd.modtaboff, cmd.nmodtab)
+
+        # if cmd.extrefsymoff == 0:
+        #     self.extrefsym = None
+        # else:
+        #     self.extrefsym = self.readsym(fh, cmd.extrefsymoff, cmd.nextrefsyms)
         #if cmd.indirectsymoff == 0:
         #    self.indirectsym = None
         #else:
@@ -70,18 +84,15 @@ class SymbolTable(object):
         #    self.locrel = self.readrel(fh, cmd.locreloff, cmd.nlocrel)
 
     def readtoc(self, fh, off, n):
-        #print 'toc', off, n
-        fh.seek(off)
+        fh.seek(self.macho_header.offset + off)
         return [dylib_table_of_contents.from_fileobj(fh) for i in xrange(n)]
 
     def readmodtab(self, fh, off, n):
-        #print 'modtab', off, n
-        fh.seek(off)
+        fh.seek(self.macho_header.offset + off)
         return [dylib_module.from_fileobj(fh) for i in xrange(n)]
 
     def readsym(self, fh, off, n):
-        #print 'sym', off, n
-        fh.seek(off)
+        fh.seek(self.macho_header.offset + off)
         refs = []
         for i in xrange(n):
             ref = dylib_reference.from_fileobj(fh)
@@ -90,6 +101,5 @@ class SymbolTable(object):
         return refs
 
     def readrel(self, fh, off, n):
-        #print 'rel', off, n
-        fh.seek(off)
+        fh.seek(self.macho_header.offset + off)
         return [relocation_info.from_fileobj(fh) for i in xrange(n)]
