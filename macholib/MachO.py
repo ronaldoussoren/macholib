58edb6875a34e034dbe98ef251f8d4a16f64e000
Taras Tsugrii <ttsugrii@fb.com>
2016-10-25 19:52:49 -0700

    Use the same dependency walk logic as otool

--- macholib/MachO.py
+++ macholib/MachO.py
@@ -24,6 +24,7 @@ __all__ = ['MachO']
 _RELOCATABLE = set((
     # relocatable commands that should be used for dependency walking
     LC_LOAD_DYLIB,
+    LC_LOAD_UPWARD_DYLIB,
     LC_LOAD_WEAK_DYLIB,
     LC_PREBOUND_DYLIB,
     LC_REEXPORT_DYLIB,
@@ -31,6 +32,7 @@ _RELOCATABLE = set((
 
 _RELOCATABLE_NAMES = {
     LC_LOAD_DYLIB: 'load_dylib',
+    LC_LOAD_UPWARD_DYLIB: 'load_upward_dylib',
     LC_LOAD_WEAK_DYLIB: 'load_weak_dylib',
     LC_PREBOUND_DYLIB: 'prebound_dylib',
     LC_REEXPORT_DYLIB: 'reexport_dylib',
